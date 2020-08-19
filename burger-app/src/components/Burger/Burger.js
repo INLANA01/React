ce2eaa1efe7ed08ca13ee83aca4fa5d5f1ef27c9
Aditya Landge <al2960@g.rit.edu>
2020-08-19 17:46:18 -0400

    custon burger module start

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -4,11 +4,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    let tranformedIngredients = Object.keys(props.ingredients).map(
+        igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key = {igKey + i} type={igKey}></BurgerIngredient>;
+            });
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if (tranformedIngredients.length === 0){
+        tranformedIngredients = <p> Please start adding ingredients! </p>
+    }
+    console.log(tranformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {tranformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
