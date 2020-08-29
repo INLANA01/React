2be85ef3861c601869aeb618374aeecffc4395de
Aditya Landge <al2960@g.rit.edu>
2020-08-29 12:26:03 -0400

    restriving data from backend

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgeBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <BurgeBuilder></BurgeBuilder>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  state = {  
+    show: true
+  };
+
+  // componentDidMount(){
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000);
+  // }
+
+  render(){
+    return (
+      <div className="App">
+        <Layout>
+          { this.state.show ? <BurgeBuilder></BurgeBuilder> : null }
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
