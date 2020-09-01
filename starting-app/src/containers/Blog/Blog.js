067063b99aa7c4c637ccf1f26a95a849e8bc2e7f
Aditya Landge <al2960@g.rit.edu>
2020-08-31 20:52:34 -0400

    setting up absolute and relative paths

--- starting-app/src/containers/Blog/Blog.js
+++ starting-app/src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 class Blog extends Component {
 
     state = {
@@ -16,8 +17,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: 'new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
@@ -33,6 +38,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=><h1>Home</h1>} />
                 <Route path="/" exact render={()=><h1>Home2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
 
             </div>
         );
--- starting-app/src/containers/Blog/Posts/Posts.js
+++ starting-app/src/containers/Blog/Posts/Posts.js
@@ -11,6 +11,7 @@ class Posts extends Component{
         this.setState( { selectedPostId: id });
     }
     componentDidMount(){
+        console.log(this.props);
         axios.get('/posts').then(
             response => {
                 const posts = response.data.slice(0,4);
@@ -40,6 +41,7 @@ class Posts extends Component{
                     key = {post.id} 
                     title={post.title} 
                     author = {post.author}
+                    match = {this.props.match}
                     clicked = {() => this.postSelectedHnadler (post.id) }></Post>;
             });
         }
